59b124822fe3a62d6d6759c4d987c64f69226bcd
justinyaoqi <yaoqiqq@hotmail.com>
2014-08-24 13:03:52 +0800

    ...
    删除垃圾

--- static副本/js/base/load-metro.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    if ((document.location.host.indexOf('.dev') > -1) || (document.location.host.indexOf('modernui') > -1) ) {
-        $("<script/>").attr('src', '/static/js/base/metro/metro-loader.js').appendTo($('head'));
-    } else {
-        $("<script/>").attr('src', '/static/js/base/metro.min.js').appendTo($('head'));
-    }
-})
